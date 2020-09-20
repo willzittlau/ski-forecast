70b4209fbbd7eef158c780f8c79db556f2f2d903
willzittlau <67138318+willzittlau@users.noreply.github.com>
2020-09-20 00:35:32 -0700

    Add DOM event listener for Jinja variables

--- static/tablecolor.js
+++ static/tablecolor.js
@@ -1,41 +1,44 @@
-const elevations = document.querySelectorAll(".elev");
-console.log(elevations)
-const dangers = document.querySelectorAll(".danger");
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const elevations = document.querySelectorAll(".elev");
+  console.log(elevations);
+  const dangers = document.querySelectorAll(".danger");
 
-elevations.forEach(function(elevation){
-  console.log(elevation);
-  switch(elevation.innerHTML){
-    case ' Alpine ':
-      elevation.style.background = 'AliceBlue';
-      break;
-    case ' Treeline ':
-      elevation.style.background = '#c1d831';
-      break;
-    case ' Below Treeline ':
-      elevation.style.background = 'SeaGreen';
-      break;
-  }
-});
+  elevations.forEach(function (elevation) {
+    console.log(elevation);
+    switch (elevation.innerHTML) {
+      case " Alpine ":
+        elevation.style.background = "AliceBlue";
+        break;
+      case " Treeline ":
+        elevation.style.background = "#c1d831";
+        break;
+      case " Below Treeline ":
+        elevation.style.background = "SeaGreen";
+        break;
+    }
+  });
 
-dangers.forEach(function(danger){
-  switch(danger.innerHTML){
-    case ' Extreme ':
-      danger.style.background = 'black';
-      danger.style.color = "white";
-      break;
-    case ' High ':
-      danger.style.background = 'red';
-      break;
-    case ' Considerable ':
-      danger.style.background = 'orange';
-      break;
-    case ' Moderate ':
-      danger.style.background = 'yellow';
-      break;
-    case ' Low ':
-      danger.style.background = 'green';
-      break;
-    default:
-      danger.style.background = 'white';
-  }
+  dangers.forEach(function (danger) {
+    switch (danger.innerHTML) {
+      case " Extreme ":
+        danger.style.background = "black";
+        danger.style.color = "white";
+        break;
+      case " High ":
+        danger.style.background = "red";
+        break;
+      case " Considerable ":
+        danger.style.background = "orange";
+        break;
+      case " Moderate ":
+        danger.style.background = "yellow";
+        break;
+      case " Low ":
+        danger.style.background = "green";
+        break;
+      default:
+        danger.style.background = "white";
+    }
+  });
 });

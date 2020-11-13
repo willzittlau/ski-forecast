1a8c662ce209bd7753230667eda1b33767da6edf
willz <willzittlau@outlook.com>
2020-11-13 11:40:41 -0800

    Misc fixes

--- static/graphs.js
+++ static/graphs.js
@@ -1,31 +1,28 @@
 //Hide / Show Graphs button
 
 function toggle_forecast() {
-  var x = document.getElementById("weathergraph");
-  if (x.innerHTML === `
-  <h3>48H Forecast (GEM-LAM, 2.5km res)</h3> 
-  {{HRDPS_plot | safe}} 
-  <p>Model elevation: {{HRDPS_elevation}}</p>
-  `) {
-    x.innerHTML === `
-    <h3>3.5 Day Forecast (NAM, 12km res)</h3>
-    {{NAM_plot | safe}}
-    <p>Model elevation: {{NAM_elevation}}</p>
-    `;
-  } else {
-    x.innerHTML = `
-    <h3>48H Forecast (GEM-LAM, 2.5km res)</h3> 
-    {{HRDPS_plot | safe}} 
-    <p>Model elevation: {{HRDPS_elevation}}</p>
-    `;
-  }
+    var x = document.getElementById("48H");
+    if (x.style.display === "none") {
+        x.style.display = "block"; 
+    } 
+    else {
+        x.style.display = "none";
+    }
+    var y = document.getElementById("72H");
+    if (x.style.display === "block") {
+        x.style.display = "none"; 
+    } 
+    else {
+        x.style.display = "block";
+    }
 }
 
 function show_historical() {
-  var x = document.getElementById("72H");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+    var x = document.getElementById("72H");
+    if (x.style.display === "none") {
+        x.style.display = "block"; 
+    } 
+    else {
+        x.style.display = "none";
+    }
 }

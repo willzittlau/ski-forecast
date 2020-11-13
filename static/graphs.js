0dcc455168e09d666e574937b6ffeb9b9d067a69
willz <willzittlau@outlook.com>
2020-11-13 11:08:41 -0800

    Fix Toggle Buttons for Graphs

--- static/graphs.js
+++ static/graphs.js
@@ -1,21 +1,25 @@
 //Hide / Show Graphs button
 
-function show_forecast() {
-    var x = document.getElementById("48H");
-    if (x.style.display === "none") {
-        x.style.display = "block"; 
-    } 
-    else {
-        x.style.display = "none";
-    }
+function toggle_forecast() {
+  var x = document.getElementById("48H");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("72H");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 function show_historical() {
-    var x = document.getElementById("72H");
-    if (x.style.display === "none") {
-        x.style.display = "block"; 
-    } 
-    else {
-        x.style.display = "none";
-    }
+  var x = document.getElementById("72H");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
